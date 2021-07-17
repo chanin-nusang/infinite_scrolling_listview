d054a13023a9d324e086741f4876d99b84e389d5
Chanin Nusang <6110110589@psu.ac.th>
2021-07-18 02:08:39 +0700

    real colors

--- lib/starwars_list.dart
+++ lib/starwars_list.dart
@@ -134,9 +134,94 @@ class _StarwarsListState extends State<StarwarsList> {
 class PeopleTile extends StatelessWidget {
   PeopleTile({@required this.people});
   final People? people;
+  static Map<String, Color?>? color = {
+    'black': Colors.black,
+    'blond': Color(0xFFFAF0BE),
+    'fair': Color(0xFFF3CFBB),
+    'blue': Colors.blue,
+    'gold': Color(0xFFd4af37),
+    'yellow': Colors.yellow,
+    'white': Colors.white,
+    'red': Colors.red,
+    'brown': Colors.brown,
+    'light': Colors.amber[100],
+    'grey': Colors.grey,
+    'auburn': Color(0xFF922704),
+    'blue-gray': Color(0xFF657895),
+    'green': Colors.green,
+    'green-tan': Color(0xFFa9be70),
+    'orange': Colors.orange,
+    'hazel': Color(0xFF9e6b4a),
+    'pale': Color(0xFFFFBFDF),
+    'metal': Color(0xFFAAA9AD),
+    'dark': Color(0xFF3A3B3C),
+    'brown mottle': Color(0xFF654321),
+    'mottled green': Color(0xFF98fb98),
+    'pink': Colors.pink,
+    'tan': Color(0xFFD2B48C),
+    'blonde': Color(0xFFF0E2B6),
+    'silver': Color(0xFFC0C0C0),
+  };
+
+  List<String>? hair, skin, eye;
+  colorSplit() async {
+    hair = people?.hairColor!.split(', ');
+    skin = people?.skinColor!.split(', ');
+    eye = people?.eyeColor!.split(', ');
+  }
+
+  List<Widget>? wid = [];
+  colorSquare() async {
+    for (int i = 0; i < hair!.length; i++) {
+      if (color!.containsKey(hair![i])) {
+        wid!.add(Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+            border: Border.all(),
+            color: color![hair![i]],
+          ),
+          height: 13,
+          width: 13,
+        ));
+      } else
+        wid!.add(Text(" - ", style: TextStyle(color: Colors.grey[600])));
+    }
+    wid!.add(Text("/", style: TextStyle(color: Colors.grey[600])));
+    for (int i = 0; i < skin!.length; i++) {
+      if (color!.containsKey(skin![i])) {
+        wid!.add(Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+            border: Border.all(),
+            color: color![skin![i]],
+          ),
+          height: 13,
+          width: 13,
+        ));
+      } else
+        wid!.add(Text(" - ", style: TextStyle(color: Colors.grey[600])));
+    }
+    wid!.add(Text("/", style: TextStyle(color: Colors.grey[600])));
+    for (int i = 0; i < eye!.length; i++) {
+      if (color!.containsKey(eye![i])) {
+        wid!.add(Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+            border: Border.all(),
+            color: color![eye![i]],
+          ),
+          height: 13,
+          width: 13,
+        ));
+      } else
+        wid!.add(Text(" - ", style: TextStyle(color: Colors.grey[600])));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    colorSplit();
+    colorSquare();
     return Container(
       padding: EdgeInsets.fromLTRB(10, 5, 10, 5),
       child: Row(
@@ -170,9 +255,16 @@ class PeopleTile extends StatelessWidget {
                   "Gender : ${people?.gender}   Height : ${people?.height.toString()}   Mass : ${people?.mass.toString()}",
                   style: TextStyle(fontSize: 15, color: Colors.grey[600]),
                 ),
-                Text(
-                    'Hair/Skin/Eye Color : ${people?.hairColor}/${people?.skinColor}/${people?.eyeColor}',
-                    style: TextStyle(color: Colors.grey[600])),
+                Row(
+                  children: [
+                    Flexible(
+                      flex: 6,
+                      child: Text('Hair/Skin/Eye Colors : ',
+                          style: TextStyle(color: Colors.grey[600])),
+                    ),
+                    for (var item in wid!) Flexible(child: item)
+                  ],
+                ),
                 Text('Year of Birth : ${people?.birthYear}',
                     style: TextStyle(
                         color: Colors.grey[600], fontWeight: FontWeight.w400))
